ca8ebf71e2486c09c712698fabcee229b359ddb3
Ranjitrevanakar02 <ranjitrevanakar02@gmail.com>
2025-05-07 16:03:01 +0530

    Update operation.java

--- operation.java
+++ operation.java
@@ -4,6 +4,6 @@ public class operation {
         int b = 20;
         System.out.println("addition:" + (a + b));
         System.out.println("addition:" + (a - b));
-        System.out.println("addition:" + (a * b));
+        System.out.println("addition:" + (a / b));
     }
 }
